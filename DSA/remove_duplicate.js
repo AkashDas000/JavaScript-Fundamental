a22080a6a7404b078384a4e75dd92c28058b5117
Akash Das <akashakki1100@gmail.com>
2025-10-09 00:39:43 +0530

    updated

---
+++ DSA/remove_duplicate.js
@@ -0,0 +1,16 @@
+let arr = [0,0,1,1,1,2,2,3,3,4]
+let n = arr.length;
+
+function removeDuplicate(arr, n){
+    let x = 0;
+    for(let i=0; i<n; i++){
+        if(arr[i] > arr[x]){
+            x = x + 1;
+            arr[x] = arr[i]
+        }
+    }
+    // console.log(arr)
+    return x+1
+}
+
+console.log(removeDuplicate(arr, n))
